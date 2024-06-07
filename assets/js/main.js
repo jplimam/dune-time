d7af226fa45b0f5a930d3989036d477b70c1963a
jplimam <limamarcelojoaopedro@gmail.com>
2024-06-07 01:59:12 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const relogio = document.querySelector('.relogio');
+let timer;
+let i;
+
+function mostraHoras(segundos) {
+    const horas = new Date(segundos * 1000);
+    return horas.toLocaleTimeString('pt-br', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
+
+function iniciaRelogio() {
+    startTime = parseInt(localStorage.getItem('cronometro')) || 0;
+    timer = setInterval(function () {
+        i++;
+        localStorage.setItem('cronometro', i);
+        relogio.innerHTML = mostraHoras(i);
+    }, 1000);
+}
+
+function pararRelogio() {
+    clearInterval(timer);
+    localStorage.removeItem('cronometro');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tempoSalvo = parseInt(localStorage.getItem('cronometro'));
+    if (tempoSalvo) {
+        i = tempoSalvo;
+        relogio.innerHTML = mostraHoras(i);
+    }
+    iniciaRelogio();
+})
